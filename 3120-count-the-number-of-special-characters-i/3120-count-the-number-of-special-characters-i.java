a81c072ff8395901b5f437dcc96192aca0166b68
Bavatharani Velusamy <bavatharanivelusamy@gmail.com>
2026-05-26 14:26:53 +0530

    Time: 3 ms (31.81%), Space: 44.1 MB (23.05%) - LeetHub

--- 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
+++ 3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.java
@@ -1,27 +1,18 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        HashMap<Character,Integer> hs=new HashMap<>();
-        HashMap<Character,Integer> hs1=new HashMap<>();
-        int c=0;
+        HashSet<Character> c=new HashSet<>();
+        HashSet<Character> l=new HashSet<>();
+        int r=0;
         for(int i=0;i<word.length();i++){
             char ch=word.charAt(i);
-            if(ch>='a'&&ch<='z') 
-            {
-                hs.put(ch,hs.getOrDefault(ch,0)+1);
-            }
-            else{
-                hs1.put(ch,hs1.getOrDefault(ch,0)+1);
-            }
-        
-        }
-        for(Map.Entry<Character,Integer> e:hs.entrySet()){
-            char ch=e.getKey();
-            char ch1=Character.toUpperCase(ch);
-            if(hs1.containsKey(ch1)){
-                c++;
-            }
-        }
-        return c;
+            if('a'<=ch && ch<='z') l.add(ch);
+            else c.add(ch);
 
+        }
+        for(Character i:l){
+            char ch=Character.toUpperCase(i);
+            if(c.contains(ch)) r++;
+        }
+        return r;
     }
 }
