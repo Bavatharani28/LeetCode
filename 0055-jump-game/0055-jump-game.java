c4cdb9392a9d9c217cb599b230ec8e9d65e4a07d
Bavatharani Velusamy <bavatharanivelusamy@gmail.com>
2026-07-07 11:09:54 +0530

    Time: 2 ms (88.83%), Space: 47.7 MB (80.8%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int reachpos=0;
+        int maxInd=0;
         for(int i=0;i<nums.length;i++){
-           if(i>reachpos){
+            if(i>maxInd)
             return false;
-           } 
-           reachpos=Math.max(reachpos,i+nums[i]);
+            maxInd=Math.max(maxInd,i+nums[i]);
         }
         return true;
     }
