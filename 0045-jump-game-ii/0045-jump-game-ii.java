d370a7e7dad72060222d9bb4edfbc771168eba7e
Bavatharani Velusamy <bavatharanivelusamy@gmail.com>
2026-07-07 11:22:43 +0530

    Time: 2 ms (31.76%), Space: 47.3 MB (38.54%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,11 +1,10 @@
 class Solution {
     public int jump(int[] nums) {
-        int j=0;
-        int cur=0;
-        int step=0;
+        int j=0,cur=0,step=0;
         for(int i=0;i<nums.length-1;i++){
-            step=Math.max(step,i+nums[i]);
-            if(i==cur){
+            step=Math.max(step,nums[i]+i);
+            if(cur==i)
+            {
                 j++;
                 cur=step;
             }
