23f9157bbd0f5cdc039dea0f2f839241baca53b6
Bavatharani Velusamy <bavatharanivelusamy@gmail.com>
2026-07-14 23:12:48 +0530

    Time: 34 ms (91.84%), Space: 53.6 MB (51.2%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,14 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    a="Hello World";
+    return function(...args) {
+        return a;
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
