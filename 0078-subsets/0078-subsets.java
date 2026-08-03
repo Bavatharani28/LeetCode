16e884dac68b8b8a2007996185b63dba8a735200
Bavatharani Velusamy <bavatharanivelusamy@gmail.com>
2026-08-03 13:51:16 +0530

    Time: 1 ms (86.83%), Space: 44.2 MB (57.96%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,21 +1,19 @@
 class Solution {
-    static void subset(List<List<Integer>> list,List<Integer> lis,int[] nums,int n,int i){
-        list.add(new ArrayList(lis));
-        if(i==n)
-        return;
-        for(int j=i;j<n;j++){
-            lis.add(nums[j]);
-            subset(list,lis,nums,n,j+1);
-            lis.remove(Integer.valueOf(nums[j]));
+    static void subsets(List<Integer> list,List<List<Integer>> llist,int i,int[] nums,int n){
+        llist.add(new ArrayList(list));
+        if(n==i){
+            return;
         }
-
-
+        for(int j=i;j<n;j++){
+            list.add(nums[j]);
+            subsets(list,llist,j+1,nums,n);
+            list.remove(Integer.valueOf(nums[j]));
+        }
     }
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> list=new ArrayList<>();
-        List<Integer> lis=new ArrayList<>();
-        int i=0;
-        subset(list,lis,nums,nums.length,i);
-        return list;
+        List<List<Integer>> longlist=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
+        subsets(list,longlist,0,nums,nums.length);
+        return longlist;
     }
 }
